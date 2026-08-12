e2aa63288f9c33b73bdce15abe6f890da8d39ae5
Paulo Clara <59574041+pjclara@users.noreply.github.com>
2026-08-12 23:54:24 +0100

    build last change

--- public/build/assets/input-error-BbUqfPCp.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-Bm-vsqnv.js";import{a as o}from"./app-logo-icon-BxJsB0MT.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
---
+++ public/build/assets/input-error-CZpbr1qM.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-dU7T8-IZ.js";import{a as o}from"./app-logo-icon-BvWSTh-X.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
